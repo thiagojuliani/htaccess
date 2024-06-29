e86c0c541eb821b3a37e25162136456e5be60e1c
Thiago Juliani <48869066+thiagojuliani@users.noreply.github.com>
2024-06-28 21:36:59 -0300

    Add files via upload

---
+++ ss.php
@@ -0,0 +1,13 @@
+<META NAME="robots" CONTENT="noindex,nofollow">
+<html><head><meta name="color-scheme" content="light dark"><meta charset="UTF-8"></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">
+                                                                                            `"""""""""`
+
+                                                                                        ./HaCked By Ramil Feyziyev
+
+                                                                                            `"""""""""`
+
+	
+                                                                                     Azerbaijan Hacking And Security Team
+		
+                                                                                      Kenan54 - Ramil Feyziyev - 0xP4nic
+</pre></body></html>
